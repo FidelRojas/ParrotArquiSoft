558ed45a15036e5c29761cac456e30fa918ecbe1
FidelRojas <fidelrojas35@gmail.com>
2020-03-17 09:41:24 -0400

    cree una interfaz parrot e implemente para European

---
+++ main/java/parrot/VelocityEuropean.java
@@ -0,0 +1,11 @@
+package parrot;
+
+public class VelocityEuropean implements VelocityCalculator {
+
+	@Override
+	public double calculateVelocity(Parrot p) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
